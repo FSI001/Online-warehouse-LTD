7cdbf80b6cd84f55732f93c8dc0a1c57fc515e79
FSI001 <112025335+FSI001@users.noreply.github.com>
2023-10-17 09:04:42 +0600

    Add files via upload

--- detect-device.js
+++ detect-device.js
@@ -1,4 +1,4 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     // Redirect mobile users to a mobile version or a blocked page
-    window.location.href = 'mobile-blocked-page.html';
+    window.location.href = '505.html';
 }
